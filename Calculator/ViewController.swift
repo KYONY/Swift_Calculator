821a9fb894941a016f92f2928b259750c0f1a0be
akademik <pvv2606@gmail.com>
2020-06-09 22:57:03 +0300

    Create Interface of Calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelResult: UILabel!
+    @IBOutlet weak var resetBotton: UIButton!
+    @IBOutlet weak var plusMinusBotton: UIButton!
+    @IBOutlet weak var persentBotton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetBotton.layer.borderWidth = 1
+        resetBotton.layer.borderColor = UIColor.gray.cgColor
+        
+        plusMinusBotton.layer.borderWidth = 1
+        plusMinusBotton.layer.borderColor = UIColor.gray.cgColor
+        
+        persentBotton.layer.borderWidth = 1
+        persentBotton.layer.borderColor = UIColor.gray.cgColor
     }
-
-
+    
+    
 }
 
